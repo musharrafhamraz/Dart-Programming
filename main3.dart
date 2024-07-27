671df77c699712338cd249520371c38d6216f673
musharrafhamraz <musharrafhamraz02@gmail.com>
2024-07-27 12:13:51 +0500

    problems for loops and conditionals.

---
+++ main3.dart
@@ -0,0 +1,15 @@
+void main() {
+  /*
+  Assignment 20
+  Write a program to print the following pattern
+  *
+  * *
+  * * *
+  * * * *
+  * * * * *
+  */
+
+  for (int i = 1; i < 5 + 1; i++) {
+    print("* " * i);
+  }
+}
