5a0abf89f708914bbdef6fb584bf63fda52696c9
musharrafhamraz <musharrafhamraz02@gmail.com>
2024-08-01 22:51:01 +0500

    added more problems.

--- logic_building.dart
+++ logic_building.dart
@@ -72,17 +72,104 @@ void main() {
   Q.  Write a program that calculates the sum of the digits of a given number.
   */
 
-  int sumNumber = 3445;
-  List<int>? isumL;
+  // int sumNumber = 3445;
+  // List<int>? isumL = [];
 
-  List sumL = sumNumber.toString().split("");
+  // List<String> sumL = sumNumber.toString().split("");
 
-  for (int i = 0; i <= sumL.length; i++) {
-    int index = int.parse(sumL[i]);
-    isumL!.add(index);
-  }
+  // for (int i = 0; i < sumL.length; i++) {
+  //   int index = int.parse(sumL[i]);
+  //   isumL.add(index);
+  // }
 
-  int sum = isumL![0] + isumL[1] + isumL[2] + isumL[3];
+  // int sum = isumL[0] + isumL[1] + isumL[2] + isumL[3];
 
-  print("the sum of $sumNumber is $sum");
+  // print("the sum of $sumNumber is $sum");
+
+  /*
+  Q.   Implement a function that reverses a given string.
+  */
+
+  // String word = "Musharraf";
+
+  // List<String> wordL = word.split("").toList();
+
+  // Iterable<String> rWordL = wordL.reversed;
+
+  // String revsersedString = rWordL.join();
+
+  // print(revsersedString);
+
+  /*
+  Q.   Write a function that counts the number of vowels and consonants in a given string.
+  */
+
+  // List<String> vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  // List<String> consonants = [
+  //   'b',
+  //   'c',
+  //   'd',
+  //   'f',
+  //   'g',
+  //   'h',
+  //   'j',
+  //   'k',
+  //   'l',
+  //   'm',
+  //   'n',
+  //   'p',
+  //   'q',
+  //   'r',
+  //   's',
+  //   't',
+  //   'v',
+  //   'w',
+  //   'x',
+  //   'y',
+  //   'z'
+  // ];
+
+  // String word = "Musharraf";
+
+  // int vowel = 0;
+  // int consonant = 0;
+
+  // List<String> wordL = word.toLowerCase().split("");
+
+  // for (int i = 0; i < wordL.length; i++) {
+  //   if (vowels.contains(wordL[i])) {
+  //     vowel++;
+  //   }
+  //   if (consonants.contains(wordL[i])) {
+  //     consonant++;
+  //   }
+  // }
+  // print("The number of Vowel in the word: $vowel");
+  // print("The number of Consonents in the word: $consonant");
+
+  /*
+  Q.   Write a function that checks if a given year is a leap year.
+  */
+
+  // int year = 2023;
+
+  // if (year % 4 == 0) {
+  //   print("The year is leap year.");
+  // } else {
+  //   print("The year is not leap year.");
+  // }
+
+  /*
+  Q.   Write a program that converts a temperature from Celsius to Fahrenheit and vice versa.
+  */
+
+  int celcs = 24;
+  int ferenh = 89;
+
+  double covToFe = celcs * 2 + 30;
+  double covToCe = (ferenh - 30) / 2;
+
+  print("The celcius to ferenheit is $covToFe");
+  print("The ferenheit to celcius is $covToCe");
 }
