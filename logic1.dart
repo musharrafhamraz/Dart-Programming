cb4108afe1099041f7f6b5048aa1bf04d06f303d
musharrafhamraz <musharrafhamraz02@gmail.com>
2024-08-06 17:35:19 +0500

    more problems added.

--- logic1.dart
+++ logic1.dart
@@ -5,33 +5,33 @@ void main() {
   Q. Write a program to calculate simple interest given principal, rate, and time.
   */
 
-  // print(
-  //     "----------------------Welcome to Interest calculator------------------------");
+  print(
+      "----------------------Welcome to Interest calculator------------------------");
 
-  // int? principal, rate, time;
+  int? principal, rate, time;
 
-  // print("Enter Principal(USD): ");
-  // principal = int.parse(stdin.readLineSync()!);
-  // print("Enter Rate(%): ");
-  // rate = int.parse(stdin.readLineSync()!);
-  // print("Enter No of Years: ");
-  // time = int.parse(stdin.readLineSync()!);
+  print("Enter Principal(USD): ");
+  principal = int.parse(stdin.readLineSync()!);
+  print("Enter Rate(%): ");
+  rate = int.parse(stdin.readLineSync()!);
+  print("Enter No of Years: ");
+  time = int.parse(stdin.readLineSync()!);
 
-  // double? interest;
-  // interest = (principal * rate * time) / 100;
+  double? interest;
+  interest = (principal * rate * time) / 100;
 
-  // print(
-  //     "The simple interest on $principal USD with a rate of $rate% in $time years is ${interest} USD");
+  print(
+      "The simple interest on $principal USD with a rate of $rate% in $time years is ${interest} USD");
 
   /*
   Q. Write a program to calculate simple interest given principal, rate, and time.
   */
 
-  // String sentence = "My name is Musharraf Hamraz";
+  String sentence = "My name is Musharraf Hamraz";
 
-  // List<String> sentenceL = sentence.split(" ");
+  List<String> sentenceL = sentence.split(" ");
 
-  // print("There are ${sentenceL.length} word in the sentence.");
+  print("There are ${sentenceL.length} word in the sentence.");
 
   /*
   Q. Write a program to calculate simple interest given principal, rate, and time.
--- logic_building.dart
+++ logic_building.dart
@@ -8,234 +8,234 @@ void main() {
       are multiples of both 3 and 5, print "FizzBuzz."
   */
 
-  // for (int i = 0; i <= 100; i++) {
-  //   if (i % 3 == 0 && i % 5 == 0) {
-  //     print("FizzBuzz");
-  //   } else if (i % 3 == 0) {
-  //     print("Fizz");
-  //   } else if (i % 5 == 0) {
-  //     print("Buzz");
-  //   } else {
-  //     print(i);
-  //   }
-  // }
+  for (int i = 0; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      print("FizzBuzz");
+    } else if (i % 3 == 0) {
+      print("Fizz");
+    } else if (i % 5 == 0) {
+      print("Buzz");
+    } else {
+      print(i);
+    }
+  }
 
   /*
   Q.  Create a function that checks whether a given string is a palindrome 
       (reads the same forwards and backwards).
   */
 
-  // String word = "dhfkh";
+  String word4 = "dhfkh";
 
-  // String reverse = word.split("").reversed.join();
+  String reverse = word4.split("").reversed.join();
 
-  // if (word.toLowerCase() == reverse.toLowerCase()) {
-  //   print("The word is palindrome.");
-  // } else {
-  //   print("The word is not palindrome.");
-  // }
+  if (word4.toLowerCase() == reverse.toLowerCase()) {
+    print("The word is palindrome.");
+  } else {
+    print("The word is not palindrome.");
+  }
 
   /*
   Q.  Write a function that calculates the factorial of a given number
   */
 
-  // int number = 5, fact = 1;
+  int number2 = 5, fact = 1;
 
-  // for (int i = 1; i <= number; i++) {
-  //   fact *= i;
-  // }
+  for (int i = 1; i <= number2; i++) {
+    fact *= i;
+  }
 
-  // print(fact);
+  print(fact);
 
   /*
   Q.  Write a function to check whether a number is prime.
   */
 
-  // bool primeCheck(int primeNumber) {
-  //   if (primeNumber <= 1) return false;
-  //   if (primeNumber == 2) return false;
-  //   if (primeNumber % 2 == 0) return false;
+  bool primeCheck(int primeNumber) {
+    if (primeNumber <= 1) return false;
+    if (primeNumber == 2) return false;
+    if (primeNumber % 2 == 0) return false;
 
-  //   int sqrtN = sqrt(primeNumber).toInt();
+    int sqrtN = sqrt(primeNumber).toInt();
 
-  //   for (int i = 3; i <= sqrtN; i += 2) {
-  //     if (primeNumber % i == 0) {
-  //       return false;
-  //     }
-  //   }
+    for (int i = 3; i <= sqrtN; i += 2) {
+      if (primeNumber % i == 0) {
+        return false;
+      }
+    }
 
-  //   return true;
-  // }
+    return true;
+  }
 
-  // print(primeCheck(82));
+  print(primeCheck(82));
 
   /*
   Q.  Write a program that calculates the sum of the digits of a given number.
   */
 
-  // int sumNumber = 3445;
-  // List<int>? isumL = [];
+  int sumNumber = 3445;
+  List<int>? isumL = [];
 
-  // List<String> sumL = sumNumber.toString().split("");
+  List<String> sumL = sumNumber.toString().split("");
 
-  // for (int i = 0; i < sumL.length; i++) {
-  //   int index = int.parse(sumL[i]);
-  //   isumL.add(index);
-  // }
+  for (int i = 0; i < sumL.length; i++) {
+    int index = int.parse(sumL[i]);
+    isumL.add(index);
+  }
 
-  // int sum = isumL[0] + isumL[1] + isumL[2] + isumL[3];
+  int sum = isumL[0] + isumL[1] + isumL[2] + isumL[3];
 
-  // print("the sum of $sumNumber is $sum");
+  print("the sum of $sumNumber is $sum");
 
   /*
   Q.   Implement a function that reverses a given string.
   */
 
-  // String word = "Musharraf";
+  String word1 = "Musharraf";
 
-  // List<String> wordL = word.split("").toList();
+  List<String> wordL1 = word1.split("").toList();
 
-  // Iterable<String> rWordL = wordL.reversed;
+  Iterable<String> rWordL = wordL1.reversed;
 
-  // String revsersedString = rWordL.join();
+  String revsersedString = rWordL.join();
 
-  // print(revsersedString);
+  print(revsersedString);
 
   /*
   Q.   Write a function that counts the number of vowels and consonants in a given string.
   */
 
-  // List<String> vowels = ['a', 'e', 'i', 'o', 'u'];
+  List<String> vowels = ['a', 'e', 'i', 'o', 'u'];
 
-  // List<String> consonants = [
-  //   'b',
-  //   'c',
-  //   'd',
-  //   'f',
-  //   'g',
-  //   'h',
-  //   'j',
-  //   'k',
-  //   'l',
-  //   'm',
-  //   'n',
-  //   'p',
-  //   'q',
-  //   'r',
-  //   's',
-  //   't',
-  //   'v',
-  //   'w',
-  //   'x',
-  //   'y',
-  //   'z'
-  // ];
+  List<String> consonants = [
+    'b',
+    'c',
+    'd',
+    'f',
+    'g',
+    'h',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
 
-  // String word = "Musharraf";
+  String word = "Musharraf";
 
-  // int vowel = 0;
-  // int consonant = 0;
+  int vowel = 0;
+  int consonant = 0;
 
-  // List<String> wordL = word.toLowerCase().split("");
+  List<String> wordL = word.toLowerCase().split("");
 
-  // for (int i = 0; i < wordL.length; i++) {
-  //   if (vowels.contains(wordL[i])) {
-  //     vowel++;
-  //   }
-  //   if (consonants.contains(wordL[i])) {
-  //     consonant++;
-  //   }
-  // }
-  // print("The number of Vowel in the word: $vowel");
-  // print("The number of Consonents in the word: $consonant");
+  for (int i = 0; i < wordL.length; i++) {
+    if (vowels.contains(wordL[i])) {
+      vowel++;
+    }
+    if (consonants.contains(wordL[i])) {
+      consonant++;
+    }
+  }
+  print("The number of Vowel in the word: $vowel");
+  print("The number of Consonents in the word: $consonant");
 
   /*
   Q.   Write a function that checks if a given year is a leap year.
   */
 
-  // int year = 2023;
+  int year = 2023;
 
-  // if (year % 4 == 0) {
-  //   print("The year is leap year.");
-  // } else {
-  //   print("The year is not leap year.");
-  // }
+  if (year % 4 == 0) {
+    print("The year is leap year.");
+  } else {
+    print("The year is not leap year.");
+  }
 
   /*
   Q.   Write a program that converts a temperature from Celsius to Fahrenheit and vice versa.
   */
 
-  // int celcs = 24;
-  // int ferenh = 89;
+  int celcs = 24;
+  int ferenh = 89;
 
-  // double covToFe = celcs * 2 + 30;
-  // double covToCe = (ferenh - 30) / 2;
+  double covToFe = celcs * 2 + 30;
+  double covToCe = (ferenh - 30) / 2;
 
-  // print("The celcius to ferenheit is $covToFe");
-  // print("The ferenheit to celcius is $covToCe");
+  print("The celcius to ferenheit is $covToFe");
+  print("The ferenheit to celcius is $covToCe");
 
   /*
   Q.   Create a function to check whether a number is an Armstrong number (a number equal to the sum of its digits 
        each raised to the power of the number of digits).
   */
 
-  // int number = 152;
-  // int fNumber = 0;
+  int number1 = 152;
+  int fNumber = 0;
 
-  // String numberS = number.toString();
+  String numberS = number1.toString();
 
-  // List<int> numberL = numberS.split("").map(int.parse).toList();
+  List<int> numberL = numberS.split("").map(int.parse).toList();
 
-  // for (int i = 0; i < numberL.length; i++) {
-  //   fNumber += pow(numberL[i], numberL.length) as int;
-  // }
+  for (int i = 0; i < numberL.length; i++) {
+    fNumber += pow(numberL[i], numberL.length) as int;
+  }
 
-  // if (number != fNumber) {
-  //   print("the number is not armstrong.");
-  // } else {
-  //   print("the number is armstrong.");
-  // }
+  if (number1 != fNumber) {
+    print("the number is not armstrong.");
+  } else {
+    print("the number is armstrong.");
+  }
 
   /*
   Q.   Write a function to check if two strings are anagrams of each other.
   */
 
-  // bool checkAnagram(String word1, String word2) {
-  //   // split to convert string to list
-  //   List<String> word1L = word1.split("");
-  //   List<String> word2L = word2.split("");
+  bool checkAnagram(String word1, String word2) {
+    // split to convert string to list
+    List<String> word1L = word1.split("");
+    List<String> word2L = word2.split("");
 
-  //   // if length of both word is not equal ultimately not anagram
-  //   if (word2L.length != word1L.length) {
-  //     return false;
-  //   }
+    // if length of both word is not equal ultimately not anagram
+    if (word2L.length != word1L.length) {
+      return false;
+    }
 
-  //   // sort the list in ascending order [a,b,c,d,......]
-  //   word2L.sort();
-  //   word1L.sort();
+    // sort the list in ascending order [a,b,c,d,......]
+    word2L.sort();
+    word1L.sort();
 
-  //   // loop through both lists to check whether every element in the list equal to another or not.
-  //   for (int i = 0; i < word1L.length; i++) {
-  //     if (word2L[i] != word1L[i]) {
-  //       return false;
-  //     }
-  //   }
-  //   return true;
-  // }
+    // loop through both lists to check whether every element in the list equal to another or not.
+    for (int i = 0; i < word1L.length; i++) {
+      if (word2L[i] != word1L[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-  // print(checkAnagram("hello", "lehlo"));
+  print(checkAnagram("hello", "lehlo"));
 
   /*
   Q.   Write a program that finds the largest and smallest numbers in a List.
   */
 
-  // List<int> numbers = [6, 5, 2, 6, 9, 2, 7, 4];
+  List<int> numbers = [6, 5, 2, 6, 9, 2, 7, 4];
 
-  // numbers.sort();
-  // int n = numbers.length - 1;
+  numbers.sort();
+  int n = numbers.length - 1;
 
-  // print("The smallest number is ${numbers[0]}");
-  // print("The smallest number is ${numbers[n]}");
+  print("The smallest number is ${numbers[0]}");
+  print("The smallest number is ${numbers[n]}");
 
   /*
   Q.   Implement a simple number guessing game where the computer randomly selects a number, 
