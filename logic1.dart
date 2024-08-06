4a9ffd049ea4a5a9c87ef7e390c9bb101ca4c85a
musharrafhamraz <musharrafhamraz02@gmail.com>
2024-08-06 17:28:42 +0500

    more problems added.

---
+++ logic1.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+void main() {
+  /*
+  Q. Write a program to calculate simple interest given principal, rate, and time.
+  */
+
+  // print(
+  //     "----------------------Welcome to Interest calculator------------------------");
+
+  // int? principal, rate, time;
+
+  // print("Enter Principal(USD): ");
+  // principal = int.parse(stdin.readLineSync()!);
+  // print("Enter Rate(%): ");
+  // rate = int.parse(stdin.readLineSync()!);
+  // print("Enter No of Years: ");
+  // time = int.parse(stdin.readLineSync()!);
+
+  // double? interest;
+  // interest = (principal * rate * time) / 100;
+
+  // print(
+  //     "The simple interest on $principal USD with a rate of $rate% in $time years is ${interest} USD");
+
+  /*
+  Q. Write a program to calculate simple interest given principal, rate, and time.
+  */
+
+  // String sentence = "My name is Musharraf Hamraz";
+
+  // List<String> sentenceL = sentence.split(" ");
+
+  // print("There are ${sentenceL.length} word in the sentence.");
+
+  /*
+  Q. Write a program to calculate simple interest given principal, rate, and time.
+  */
+}
