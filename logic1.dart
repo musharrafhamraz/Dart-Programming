5bec07a09f155347210d52b196eefe2b9c681c30
musharrafhamraz <musharrafhamraz02@gmail.com>
2024-08-09 14:14:51 +0500

    ...

--- logic1.dart
+++ logic1.dart
@@ -5,35 +5,67 @@ void main() {
   Q. Write a program to calculate simple interest given principal, rate, and time.
   */
 
-  print(
-      "----------------------Welcome to Interest calculator------------------------");
+  // print(
+  //     "----------------------Welcome to Interest calculator------------------------");
 
-  int? principal, rate, time;
+  // int? principal, rate, time;
 
-  print("Enter Principal(USD): ");
-  principal = int.parse(stdin.readLineSync()!);
-  print("Enter Rate(%): ");
-  rate = int.parse(stdin.readLineSync()!);
-  print("Enter No of Years: ");
-  time = int.parse(stdin.readLineSync()!);
+  // print("Enter Principal(USD): ");
+  // principal = int.parse(stdin.readLineSync()!);
+  // print("Enter Rate(%): ");
+  // rate = int.parse(stdin.readLineSync()!);
+  // print("Enter No of Years: ");
+  // time = int.parse(stdin.readLineSync()!);
 
-  double? interest;
-  interest = (principal * rate * time) / 100;
+  // double? interest;
+  // interest = (principal * rate * time) / 100;
 
-  print(
-      "The simple interest on $principal USD with a rate of $rate% in $time years is ${interest} USD");
-
-  /*
-  Q. Write a program to calculate simple interest given principal, rate, and time.
-  */
-
-  String sentence = "My name is Musharraf Hamraz";
-
-  List<String> sentenceL = sentence.split(" ");
-
-  print("There are ${sentenceL.length} word in the sentence.");
+  // print(
+  //     "The simple interest on $principal USD with a rate of $rate% in $time years is ${interest} USD");
 
   /*
   Q. Write a program to calculate simple interest given principal, rate, and time.
   */
+
+  // String sentence = "My name is Musharraf Hamraz";
+
+  // List<String> sentenceL = sentence.split(" ");
+
+  // print("There are ${sentenceL.length} word in the sentence.");
+
+  /*
+  Q. Write a function to implement a function that returns the nth Fibonacci number
+  */
+
+  // int n = 10;
+  // int a = 0;
+  // int b = 1;
+  // int fib = 1;
+
+  // for (int i = 2; i <= n; i++) {
+  //   fib = a + b;
+  //   a = b;
+  //   b = fib;
+  // }
+
+  // print("The $n th Fibonaci Sequence is $fib");
+
+  int fiboSeq(int n) {
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+
+    int a = 0;
+    int b = 1;
+    int fib = 1;
+
+    for (int i = 2; i <= n; i++) {
+      fib = a + b;
+      a = b;
+      b = fib;
+    }
+    print("The $n-th fibo seq is $fib");
+    return fib;
+  }
+
+  fiboSeq(10);
 }
